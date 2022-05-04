4445123e59002b86ad4eff98ccbe560c0c28ec4f
adeelshahzad9911 <91594257+adeelshahzad9911@users.noreply.github.com>
2022-05-04 15:25:48 +0500

    Add files via upload

---
+++ rental_app_customize/__manifest__.py
@@ -0,0 +1,19 @@
+
+{
+    "name": "Rental App Customize",
+    'version': '13.6',
+    'license': 'LGPL-3',
+    'category': 'Rental',
+    'sequence': 3,
+    'summary': "Rental App Customize",
+    'author': 'none',
+    'company': 'Technology Solutions',
+    'website': "http://www.globalxs.co",
+    "depends": ['sale_renting', 'sale'],
+    "data": [
+        'views/sale_order.xml',
+    ],
+    "installable": True,
+    "auto_install": False,
+    'application': True,
+}
